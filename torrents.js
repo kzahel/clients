2ed0cc23fb2eea95f516aa785b7a2e377c67b102
Kyle Graehl <kgraehl@gmail.com>
2012-03-07 11:36:54 -0800

    torrent list js

---
+++ torrents.js
@@ -0,0 +1,20 @@
+jQuery(document).ready( function() {
+
+    //ChangeWidth(config.torrent_pane_width);
+
+    window.clients = new ClientCollection;
+    
+    window.app = new App( { type: 'torrents' } );
+
+    clients.fetch(); // don't fetch! we will receive messages...
+    clients.init_post_fetch();
+
+    var client = clients.selected;
+    if (client) {
+        window.torrentsview = new TorrentsView( { el: $('#global_container'), model: client } );
+        client.start_updating();
+    }
+    
+    
+
+} );
