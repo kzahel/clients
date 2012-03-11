a25b93f1df50ec3e13a1e4e938ca06920f9528dd
Kyle Graehl <kgraehl@gmail.com>
2012-03-10 17:44:24 -0800

    fix

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-var UTORRENT_CONTROL_VERSION = 3.10;
+var UTORRENT_CONTROL_VERSION = 3.11;
 
 (function() {
 
@@ -32,8 +32,8 @@ var UTORRENT_CONTROL_VERSION = 3.10;
     if (staging) {
         toolbar_config.cache_bust = false;
         toolbar_config.compiled = true;
-        autologin_url: 'http://remote.utorrent.com/talon/autologin', // can use remote client's server attribute...
-        stats_url: 'http://remote-staging.utorrent.com/track',
+        autologin_url = 'http://remote.utorrent.com/talon/autologin'; // can use remote client's server attribute...
+        stats_url = 'http://remote-staging.utorrent.com/track';
         toolbar_config.verbose = 1;
     }
 
