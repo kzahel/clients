eaf6acf64aa1634b5a350eadc6c78319a4ef8a0f
unknown <aak@.atwss.com>
2012-03-27 17:21:33 +0300

    automate updating uquest.js by injected scripts

---
+++ jsinject/cmd_update_uquest.js.cmd
@@ -0,0 +1,4 @@
+C:\Python27\python jscompress.py --input=uquest.init_jquery.js --output="..\uquest.js" --varname="    _script_init_jquery: "
+C:\Python27\python jscompress.py --input=uquest.inject.js --output="..\uquest.js" --varname="    _script_init: "
+C:\Python27\python jscompress.py --input=uquest.inject.setactive.js --output="..\uquest.js" --varname="    _script_set_active: "
+C:\Python27\python jscompress.py --input=uquest.inject.setinactive.js --output="..\uquest.js" --varname="    _script_set_inactive: "
--- jsinject/jscompress.py
+++ jsinject/jscompress.py
@@ -1,28 +1,53 @@
 import getopt
 import sys
+import fileinput
 
-input_file = ''
-output_filename = 'default.out'
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
 
-print 'ARGV      :', sys.argv[1:]
+def main():
+    input_file = ''
+    output_filename = 'default.out'
+    script_var_name = None
 
-options, rem = getopt.gnu_getopt(sys.argv[1:], 'o:v', ['input=',
-                                                             'output=', 
-                                                             ])
-print 'OPTIONS   :', options
+    print 'ARGV      :', sys.argv[1:]
 
-for opt, arg in options:
-    if opt in ('-i', '--input'):
-       input_file = arg
-    elif opt in ('-o', '--output'):
-        output_filename = arg
+    options, rem = getopt.gnu_getopt(sys.argv[1:], 'o:v', ['input=',
+                                                           'output=',
+                                                           'varname=',
+                                                          ])
+    print 'OPTIONS   :', options
 
-print 'INPUT     :', input_file
-print 'OUTPUT    :', output_filename
+    for opt, arg in options:
+        if opt in ('-i', '--input'):
+           input_file = arg
+        elif opt in ('-o', '--output'):
+            output_filename = arg
+        elif opt in ('-v', '--varname'):
+            script_var_name = arg
 
-linestring = open(input_file, 'r').read()
-f = open(output_filename, 'a')
-f.write("/* " + input_file + " */\n\"")
-f.write(linestring.replace('\n', "\\n"))
-f.write("\"\n\n")
-f.close()
+    print 'INPUT     :', input_file
+    print 'OUTPUT    :', output_filename
+    print 'NAME      :', script_var_name
+
+
+    linestring = open(input_file, 'r').read()
+    
+    if script_var_name is None:
+        outstring = ''.join(["/* ", input_file, " */\n", 
+                             "\"", linestring.replace('\n', "\\n"), "\"\n\n"])
+        f = open(output_filename, 'a')
+        f.write(outstring)
+        f.close()
+    else :
+        outstring = ''.join([script_var_name, " \"", linestring.replace('\n', "\\n"), "\",\n"])
+	for line in fileinput.input(output_filename, inplace=1):
+            if script_var_name in line:
+                line = outstring
+            sys.stdout.write(line)
+
+if __name__ == "__main__":
+    main()
