7c5c3a49603042362b8781beafee82ccf66eed57
unknown <aak@.atwss.com>
2012-03-28 15:36:57 +0300

    js compress util fix

--- jsinject/jscompress.py
+++ jsinject/jscompress.py
@@ -1,16 +1,10 @@
-import getopt
 import sys
+import getopt
 import fileinput
 
-def replaceAll(file,searchExp,replaceExp):
-    for line in fileinput.input(file, inplace=1):
-        if searchExp in line:
-            line = line.replace(searchExp,replaceExp)
-        sys.stdout.write(line)
-
 def main():
-    input_file = ''
-    output_filename = 'default.out'
+    input_file_name = ''
+    output_file_name = 'default.out'
     script_var_name = None
 
     print 'ARGV      :', sys.argv[1:]
@@ -23,28 +17,29 @@ def main():
 
     for opt, arg in options:
         if opt in ('-i', '--input'):
-           input_file = arg
+           input_file_name = arg
         elif opt in ('-o', '--output'):
-            output_filename = arg
+            output_file_name = arg
         elif opt in ('-v', '--varname'):
             script_var_name = arg
 
-    print 'INPUT     :', input_file
-    print 'OUTPUT    :', output_filename
+    print 'INPUT     :', input_file_name
+    print 'OUTPUT    :', output_file_name
     print 'NAME      :', script_var_name
 
+    with open(input_file_name, 'r') as f:
+        input_string = f.read()
 
-    linestring = open(input_file, 'r').read()
+    output_string = input_string.replace('\n', "\\n")
     
     if script_var_name is None:
-        outstring = ''.join(["/* ", input_file, " */\n", 
-                             "\"", linestring.replace('\n', "\\n"), "\"\n\n"])
-        f = open(output_filename, 'a')
-        f.write(outstring)
-        f.close()
+        outstring = ''.join(["/* ", input_file_name, " */\n", 
+                             "\"", output_string, "\"\n\n"])
+        with open(output_filename, 'a') as f:
+            f.write(outstring)
     else :
-        outstring = ''.join([script_var_name, " \"", linestring.replace('\n', "\\n"), "\",\n"])
-	for line in fileinput.input(output_filename, inplace=1):
+        outstring = ''.join([script_var_name, " \"", output_string, "\",\n"])
+	for line in fileinput.input(output_file_name, inplace=1):
             if script_var_name in line:
                 line = outstring
             sys.stdout.write(line)
