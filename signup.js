e3e575a10cf839d476c423d192113c567f46f687
Kyle Graehl <kgraehl@gmail.com>
2012-04-10 15:35:01 -0700

    fix

--- signup.js
+++ signup.js
@@ -6,6 +6,35 @@ jQuery(document).ready( function() {
 
     window.app = new App( { type: "signup" } );
 
+    function doset(client, username, password) {
+        client.set_settings( { 'webui.uconnect_username': username,
+                               'webui.uconnect_password': password },
+                             function() {
+
+                                 client.set_settings( { 'webui.uconnect_enable': 0 },
+                                                      function() {
+
+                                                          client.set_settings( { 'webui.uconnect_enable': 1 },
+                                                                               function() {
+
+                                                                                   client.set('remote_username', username);
+                                                                                   client.save();
+                                                                                   dostatus('done!');
+                                                                                   setTimeout( function() {
+                                                                                       app.broadcast( { message: 'reset' } );
+                                                                                       BTCloseFloatingWindow();
+                                                                                   }, 200);
+                                                                               },
+                                                                               onerr
+                                                                             );
+                                                      },
+                                                      onerr );
+                             },
+                             onerr );
+
+    }
+
+
     var args = decode_url_arguments();
 
     $('.dialog_head h2').text('Sign up');
@@ -19,7 +48,9 @@ jQuery(document).ready( function() {
                           if (data && data.settings) {
                               for (var i=0; i<data.settings.length; i++) {
                                   if (data.settings[i][0] == 'webui.uconnect_username') {
-                                      $('#username').val( data.settings[i][2] );
+                                      var val = data.settings[i][2];
+                                      client.set('uconnect_username', val);
+                                      $('#username').val( val );
                                       break;
                                   }
                               }
@@ -52,64 +83,28 @@ jQuery(document).ready( function() {
         }
 
         var session = new falcon.session;
-        dostatus('Checking username');
-        debugger;
-        session.check_username( username, {
-            success: function(data, status, xhr) {
-                // need to disable and then enable, I think...
 
-                if (data.code == '/ok' && data.exists === false) {
-                    client.set_settings( { 'webui.uconnect_username': username,
-                                           'webui.uconnect_password': password },
-                                         function() {
-                                             debugger;
-                                             client.set_settings( { 'webui.uconnect_enable': 0 },
-                                                                  function() {
-                                                                      debugger;
-                                                                      client.set_settings( { 'webui.uconnect_enable': 1 },
-                                                                                           function() {
-                                                                                               debugger;
-                                                                                               client.set('remote_username', username);
-                                                                                               client.save();
-                                                                                               dostatus('done!');
-                                                                                               setTimeout( function() {
-                                                                                                   app.broadcast( { message: 'reset' } );
-                                                                                                   BTCloseFloatingWindow();
-                                                                                               }, 200);
-                                                                                           },
-                                                                                           onerr
-                                                                                         );
-                                                                  },
-                                                                  onerr );
-                                         },
-                                         onerr );
+        var cur_username = client.get('uconnect_username');
+        if (cur_username == username) {
+            doset(client, username, password);
+        } else {
+            dostatus('Checking username');
+            session.check_username( username, {
+                success: function(data, status, xhr) {
+                    // need to disable and then enable, I think...
 
-/*
-                    client.doreq( 'action=setsetting&s=webui.uconnect_username&v=' + encodeURIComponent(username) +
-                                  '&s=webui.uconnect_password&v=' + encodeURIComponent(password) +
-                                  '&s=webui.uconnect_enable&v=1',
-                                  function(data, status, xhr) {
-                                      if (data && data.build) {
-                                          dostatus('done!');
-                                          BTCloseFloatingWindow();
-                                          // good to go!
-                                      } else {
-                                          onerr(data);
-                                      }
-                                  },
-                                  function(xhr, status, text) {
-                                      debugger;
-                                      onerr(text);
-                                  });
-*/
-                } else {
+                    if (data.code == '/ok' && data.exists === false) {
+                        doset(client, username, password);
+
+                    } else {
+                        onerr('username taken');
+                    }
+                },
+                error: function(xhr, status, text) {
                     onerr('username taken');
                 }
-            },
-            error: function(xhr, status, text) {
-                onerr('username taken');
-            }
-        });
+            });
+        }
     }
 
     $('#button_login').click( do_signup );
