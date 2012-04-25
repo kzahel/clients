6ce61101b8574ea14573d75024d3e0480fffae08
Andrei Kaparykha <andrei.kaparykha@gmail.com>
2012-04-25 16:14:12 +0300

    merge uquest.init_jquery.js and uquest.inject.js

--- jsinject/cmd_update_uquest.js.cmd
+++ jsinject/cmd_update_uquest.js.cmd
@@ -1,4 +1,3 @@
-C:\Python27\python jsinline.py --input=uquest.init_jquery.js --output="..\uquest.js" --varname="    _script_init_jquery: "
 C:\Python27\python jsinline.py --input=uquest.inject.js --output="..\uquest.js" --varname="    _script_init: "
 C:\Python27\python jsinline.py --input=uquest.inject.setactive.js --output="..\uquest.js" --varname="    _script_set_active: "
 C:\Python27\python jsinline.py --input=uquest.inject.setinactive.js --output="..\uquest.js" --varname="    _script_set_inactive: "
